d2fd26d4d5877c99e8573898bc09ffecb95851d0
Andrii Suboch <andrii.suboch@gmail.com>
2021-01-14 16:00:34 +0200

    every div has own hyperlink

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,26 +4,28 @@ import './Card.scss';
 
 export const Card = ({ result })=> {
   const [isOpen, setOpenValue] = useState(false);
-  const { name, description, language } = result;
+  const { name, description, language, html_url } = result;
 
   return (
-    <div className="card">
-      <h3 className="card__heading">{name}</h3>
-      <p className="card__lang">
-        Language: <span>{language}</span>
-      </p>
-      <p
-        className="card__desc"
-        style={{
-          overflow: "hidden",
-          textOverflow: "ellipsis",
-          whiteSpace: isOpen ? "normal" : "nowrap",
-        }}
-        onClick={() => setOpenValue(!isOpen)}
-      >
-        Description: <span>{description}</span>
-      </p>
-    </div>
+    <a href={html_url}>
+      <div className="card">
+        <h3 className="card__heading">{name}</h3>
+        <p className="card__lang">
+          Language: <span>{language}</span>
+        </p>
+        <p
+          className="card__desc"
+          style={{
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            whiteSpace: isOpen ? "normal" : "nowrap",
+          }}
+          onClick={() => setOpenValue(!isOpen)}
+        >
+          Description: <span>{description}</span>
+        </p>
+      </div>
+    </a>
   );
 };
 
@@ -32,6 +34,7 @@ Card.propTypes = {
     name: PropTypes.string.isRequired,
     description: PropTypes.string,
     language: PropTypes.string,
+    html_url: PropTypes.string.isRequired,
   }).isRequired
 };
 
